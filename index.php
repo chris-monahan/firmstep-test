523c852e34f68338eb3b91bb01ff1933e943ec7f
Christopher Monahan <cmonahan5@gmail.com>
2017-12-08 11:25:07 +0000

    Fix for loading issue + NetBeans config

--- index.php
+++ index.php
@@ -3,18 +3,26 @@
 namespace QueueApp;
 
 //uncomment to view warnings
-error_reporting(E_ERROR | E_PARSE);
+//error_reporting(E_ERROR | E_PARSE);
 
+require("Response.php");
 
 function requireAll($dirName, $onceLimit = true){
-	$dir = new \DirectoryIterator($dirName);
+        
+    //echo "requireAll('.')";
+    
+	$dir = new \DirectoryIterator($dirName); 
 	foreach ($dir as $fileinfo) {
+            
+                //var_dump($fileinfo);
 		
 		if 	((!$fileinfo->isDot()) && 
 			($fileinfo->isReadable()) && 
 			($fileinfo->isFile()) ) {
+                    
+                        
 				
-			$fullFileName = $fileinfo->getPathname();#
+			$fullFileName = $fileinfo->getPathname(); 
 			if(substr($fullFileName, -4) === ".php"){
 				if($onceLimit === true){
 					require_once($fullFileName);
